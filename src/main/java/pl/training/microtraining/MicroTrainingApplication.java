fc5e389ce1f301589b38e0991d967c5d6556aaa0
mariusz.kuc <mariuszkuc1@gmail.com>
2023-09-12 13:14:58 +0200

    init

---
+++ src/main/java/pl/training/microtraining/MicroTrainingApplication.java
@@ -0,0 +1,12 @@
+package pl.training.microtraining;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroTrainingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MicroTrainingApplication.class, args);
+  }
+}
---
+++ src/test/java/pl/training/microtraining/MicroTrainingApplicationTests.java
@@ -0,0 +1,11 @@
+package pl.training.microtraining;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroTrainingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
